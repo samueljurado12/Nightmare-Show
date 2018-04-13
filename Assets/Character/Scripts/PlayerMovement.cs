62ae1699bec5d007686256fdb2fbe96a3c0a06b9
samueljurado12 <samueljurado12@gmail.com>
2018-04-13 23:48:48 +0200

    Removed DOTWeen

---
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]private float speed, jumpForce;
+	[Range(1,2)]
+	[SerializeField]private int playerNumber;
+
+	private bool onGround;
+
+	// Use this for initialization
+	void Start () {
+		onGround = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move (){
+		transform.Translate(Input.GetAxis("Horizontal" + playerNumber) * speed * Time.deltaTime * Vector3.right);
+	}
+}
