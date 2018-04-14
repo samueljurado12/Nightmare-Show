4db83a1d0dad269c2b7aa7f77f46a6e96ac12f0b
David Ponce <zolastrodavid@gmail.com>
2018-04-14 10:08:19 +0200

    Created ScoreManager

---
+++ Assets/Services/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager {
+
+    public static int playerAScore = 0;
+    public static int playerBScore = 0;
+
+    public static void ResetScore() {
+        playerAScore = 0;
+        playerBScore = 0;
+    }
+}
