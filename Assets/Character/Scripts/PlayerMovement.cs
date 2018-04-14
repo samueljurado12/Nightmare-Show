3296d5d6ad9280f1634cfd9829c60918b2010eb0
samueljurado12 <samueljurado12@gmail.com>
2018-04-14 12:00:56 +0200

    Basic movement finishedand polished

--- Assets/Character/Scripts/PlayerMovement.cs
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -4,41 +4,102 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	[SerializeField]private float speed, jumpForce, gravityForce;
-	[Range(1,2)]
-	[SerializeField]private int playerNumber;
+	enum PlayerState {STAND, WALK, JUMP};
+	[Range (1, 2)]
+	[SerializeField]private int playerNumber = 1;
+	[SerializeField]private float walkSpeed, jumpSpeed, minJumpForce, maxFallSpeed, gravityForce;
 
-	private bool onGround;
+	private Vector2 velocity;
+	private Vector3 scale;
+	private bool onGround, pushingWallLeft, pushingWallRight, againstCeiling;
+	private PlayerState currentState;
 
 	// Use this for initialization
 	void Start () {
+		velocity = Vector2.zero;
+		currentState = PlayerState.STAND;
 		onGround = false;
+		pushingWallLeft = false;
+		pushingWallRight = false;
+		againstCeiling = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float horizontalDir = Input.GetAxis ("Horizontal" + playerNumber);
-		if (!onGround) {
-			Gravity ();
-		} else {
-			Jump (horizontalDir);
-			Move (horizontalDir);
+		switch (currentState) {
+		case PlayerState.STAND:
+			velocity = Vector2.zero;
+				//TODO Insert animations
+			if (!onGround) {
+				currentState = PlayerState.JUMP;
+				break;
+			}
+			if (horizontalDir != 0) {
+				currentState = PlayerState.WALK;
+				break;
+			} else if (Input.GetButton ("Jump" + playerNumber)) {
+				velocity.y = jumpSpeed;
+				currentState = PlayerState.JUMP;
+				break;
+			}
+			break;
+
+		case PlayerState.WALK:
+			//TODO Insert animations
+			if (horizontalDir == 0) {
+				currentState = PlayerState.STAND;
+				velocity = Vector2.zero;
+				break;
+			} else {
+				velocity.x = (pushingWallLeft && horizontalDir < 0f||
+				pushingWallRight && horizontalDir > 0f) ? 
+					0f : horizontalDir * walkSpeed;
+				//TODO Add scale to reverse sprite
+			}
+
+			if (Input.GetButton ("Jump" + playerNumber)) {
+				velocity.y = jumpSpeed;
+				//TODO Add audio(?)
+				currentState = PlayerState.JUMP;
+				break;
+			} else if (!onGround) {
+				currentState = PlayerState.JUMP;
+				break;
+			}
+			break;
+		case PlayerState.JUMP:
+			//TODO Insert animations
+			velocity.y -= gravityForce * Time.deltaTime;
+			velocity.y = Mathf.Max (velocity.y, -maxFallSpeed);
+			if (horizontalDir == 0) {
+				velocity.x = 0;
+			} else {
+				velocity.x = (pushingWallLeft && horizontalDir < 0f ||
+				pushingWallRight && horizontalDir > 0f) ? 
+					0f : horizontalDir * walkSpeed;
+				//TODO Add scaling to reverse sprite
+			}
+			if (!Input.GetButton ("Jump" + playerNumber) && velocity.y > 0.0f)
+				velocity.y = Mathf.Min (velocity.y, minJumpForce);
+			if (onGround) {
+				if (horizontalDir == 0) {
+					currentState = PlayerState.STAND;
+					velocity = Vector2.zero;
+				} else {
+					currentState = PlayerState.WALK;
+					velocity.y = 0;
+				}
+				//TODO Insert audio(?)
+				break;
+			}
+			break;
 		}
+		Debug.Log (velocity);
+		transform.Translate (velocity * Time.deltaTime);
 	}
 
-	void Move (float direction){
-		transform.Translate(direction * speed * Time.deltaTime * Vector2.right);
-	}
-
-	void Jump (float direction) {
-		//TODO Implement method
-	}
-
-	void Gravity () {
-		transform.Translate (Vector2.down * gravityForce * Time.deltaTime);
-	}
-
-	void OnCollisionStay2D(Collision2D col){
+	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.CompareTag ("Floor")) {
 			onGround = true;
 		}
