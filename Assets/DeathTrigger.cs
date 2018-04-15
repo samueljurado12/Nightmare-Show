946c1ab5db7b2ec4f44c92eac88a62c6d1aaa9ef
samueljurado12 <samueljurado12@gmail.com>
2018-04-15 12:15:46 +0200

    Fixed clipping in platforms

---
+++ Assets/DeathTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.CompareTag ("Phobia")) {
+			Destroy (col.gameObject);
+		}
+	}
+}
