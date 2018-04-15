c74907eefef0e9471c80326fb119157cc98d0b64
Alvaro de Marcos <almaral4c@gmail.com>
2018-04-15 10:56:19 +0200

    Added Transition in loosing scenes and Animation in Player1 death.

---
+++ Assets/BackToMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMain : MonoBehaviour {
+
+	void Start () {
+		InvokeRepeating ("GoToMain", 5, 1 * Time.deltaTime);
+	}
+
+	void GoToMain () {
+		if (Input.GetButtonDown ("Submit")) {
+			GameSceneManager.LoadMainMenu ();
+		}
+	}
+}
