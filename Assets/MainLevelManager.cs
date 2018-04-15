17ddcb92434fa1f1d3d52234b813577bd19c06ca
Alvaro de Marcos <almaral4c@gmail.com>
2018-04-15 13:46:27 +0200

    Added Credits, Controls, Main menu. SceneManager change and buildSettings.

---
+++ Assets/MainLevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLevelManager : MonoBehaviour {
+
+	public void GoToPlay () {
+		GameSceneManager.LoadNextRandomLevel ();
+	}
+
+	public void GoToCredits () {
+		GameSceneManager.LoadCredits ();
+	}
+
+	public void GoToControls () {
+		GameSceneManager.LoadControls ();
+	}
+}
