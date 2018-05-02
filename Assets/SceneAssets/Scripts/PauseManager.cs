0e22320a433e71a91208fbb2d3e4a47d2f21303b
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-05-02 21:56:36 +0200

    Added simple pause

---
+++ Assets/SceneAssets/Scripts/PauseManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+
+    private bool isGamePaused;
+
+	// Use this for initialization
+	void Start () {
+        isGamePaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Pause") || (Input.GetButtonDown("Cancel") && isGamePaused)) {
+            UpdatePauseState();
+        }
+	}
+
+    private void UpdatePauseState() {
+        if (!isGamePaused) {
+            Time.timeScale = 0;
+            isGamePaused = true;
+        } else {
+            isGamePaused = false;
+            Time.timeScale = 1;
+        }
+    }
+}
